23bc907c5a5c7e8ac16b98120c1c58c686789ed0
cjpanda <verifiedceejay@gmail.com>
2024-02-19 17:17:52 +0100

    form fixed

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
         {/* Form and Image Container */}
         <div className="flex flex-col xl:flex-row justify-center items-center xl:justify-between h-full gap-[50px] w-full">
           {/* Image  */}
-          {/* <div className="pt-10 rounded-[20px] padding-y ">
+          <div className="pt-10 rounded-[20px] padding-y ">
             <Image
               src="/denosh.jpg"
               alt=""
@@ -19,77 +19,74 @@ const Contact = () => {
               height={200}
               className="object-contain rounded-[50px]"
             />
-          </div> */}
+          </div>
           {/* Form */}
-          <form className="flex flex-col justify-center items-center gap-y-5 w-full paddding-y padding-x pt-10 pb-10">
-            <h1 className="h2 text-primary text-center xl:text-left">
-              Get In <span className="text-secondary">Touch With Us</span>
-            </h1>
-            <fieldset
-              id="form"
-              className="flex flex-col w-full gap-y-5 text-primary font-semibold shadow-xl p-5 rounded-[20px] "
-            >
-              <div className="flex flex-col ">
-                <label htmlFor="full-name">Full Name:</label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Full Name"
-                  required
-                  className="bg-background outline-none h-10 border rounded-lg pl-2 focus:border-primary"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="email">Email Address:</label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email Address"
-                  required
-                  className="bg-background outline-none h-10 border rounded-lg pl-2 focus:border-primary"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="location">Phone Number:</label>
-                <input
-                  type="text"
-                  name="location"
-                  placeholder="08023456398"
-                  required
-                  className="bg-background outline-none h-10 border rounded-lg pl-2 focus:border-primary"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="location">Location:</label>
-                <input
-                  type="text"
-                  name="location"
-                  placeholder="Example:Lagos"
-                  required
-                  className="bg-background outline-none h-10 border rounded-lg pl-2 focus:border-primary"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="message">Message:</label>
-                <textarea
-                  name="message"
-                  id="message"
-                  rows={10}
-                  cols={30}
-                  placeholder="Message"
-                  required
-                  className="bg-background outline-none h-10 border rounded-lg pl-2 pt-2 focus:border-primary"
-                ></textarea>
-              </div>
-            </fieldset>
-            <CustomButton
-              title="Submit"
-              btnType="submit"
-              containerStyles="bg-primary hover:bg-primary-hover text-white rounded-xl mt-10 uppercase btn-lg btn "
-            />
-          </form>
+          <div className="w-1/8">
+            <form className="flex flex-col justify-center items-center gap-y-5 w-full paddding-y padding-x pt-10 pb-10">
+              <h1 className="h2 text-primary text-center xl:text-left">
+                Get In <span className="text-secondary">Touch With Us</span>
+              </h1>
+              <fieldset
+                id="form"
+                className="flex flex-col w-full gap-y-5 text-primary font-semibold shadow-xl p-5 rounded-[20px] "
+              >
+                <div className="flex flex-col ">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Full Name"
+                    required
+                    className="bg-background outline-none h-10 border rounded-lg pl-2 focus:border-primary border-primary border-opacity-50 "
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email Address"
+                    required
+                    className="bg-background outline-none h-10 border rounded-lg pl-2 focus:border-primary  border-primary border-opacity-50"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <input
+                    type="text"
+                    name="location"
+                    placeholder="Phone Number"
+                    required
+                    className="bg-background outline-none h-10 border rounded-lg pl-2 focus:border-primary  border-primary border-opacity-50"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <input
+                    type="text"
+                    name="location"
+                    placeholder="Location"
+                    required
+                    className="bg-background outline-none h-10 border rounded-lg pl-2 focus:border-primary  border-primary border-opacity-50"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows={10}
+                    cols={30}
+                    placeholder="Message"
+                    required
+                    className="bg-background outline-none h-10 border rounded-lg pl-2 pt-2 focus:border-primary  border-primary border-opacity-50"
+                  ></textarea>
+                </div>
+              </fieldset>
+              <CustomButton
+                title="Submit"
+                btnType="submit"
+                containerStyles="bg-primary hover:bg-primary-hover text-white rounded-xl mt-10 uppercase btn-lg btn "
+              />
+            </form>
+          </div>
         </div>
       </div>
     </section>
